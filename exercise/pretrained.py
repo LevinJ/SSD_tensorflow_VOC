df568ec25346c55efff3d5c2a402e24263edd6ea
Levin Jian <jianzhirong@gmail.com>
2017-03-31 15:10:37 +0800

    use vgg16

--- exercise/pretrained.py
+++ exercise/pretrained.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 
 from datasets import imagenet
 from nets import inception
+from nets import vgg
 from preprocessing import inception_preprocessing
+from preprocessing import vgg_preprocessing
 
 import tensorflow.contrib.slim as slim
 from utility.dumpload import DumpLoad
@@ -46,12 +48,13 @@ class PreTrained():
             plt.figure()
             plt.imshow(np_image.astype(np.uint8))
             plt.axis('off')
+            plt.title(img_path)
             plt.show()
             
             
         
         return
-    def disp_names(self, sorted_inds,probabilities):
+    def disp_names(self, sorted_inds,probabilities, include_background=True):
         dump_load = DumpLoad("../../data/imagenet/imagenet_labels_dict.pickle")
         if dump_load.isExisiting():
             names = dump_load.load()
@@ -61,10 +64,49 @@ class PreTrained():
             
         for i in range(5):
             index = sorted_inds[i]
-            print('Probability %0.2f%% => [%s]' % (probabilities[index], names[index]))
+            if include_background:
+                print('Probability %0.2f%% => [%s]' % (probabilities[index], names[index]))
+            else:
+                print('Probability %0.2f%% => [%s]' % (probabilities[index], names[index+1]))
+        return
+    def use_vgg16(self):
+        
+        with tf.Graph().as_default():
+            image_size = vgg.vgg_16.default_image_size
+            img_path = "../../data/misec_images/First_Student_IC_school_bus_202076.jpg"
+            checkpoint_path = "../../data/trained_models/vgg16/vgg_16.ckpt"
+            
+            image_string = tf.read_file(img_path)
+            image = tf.image.decode_jpeg(image_string, channels=3)
+            processed_image = vgg_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+            processed_images  = tf.expand_dims(processed_image, 0)
+            
+            # Create the model, use the default arg scope to configure the batch norm parameters.
+            with slim.arg_scope(vgg.vgg_arg_scope()):
+                # 1000 classes instead of 1001.
+                logits, _ = vgg.vgg_16(processed_images, num_classes=1000, is_training=False)
+                probabilities = tf.nn.softmax(logits)
+                
+                init_fn = slim.assign_from_checkpoint_fn(
+                    checkpoint_path,
+                    slim.get_model_variables('vgg_16'))
+                
+                with tf.Session() as sess:
+                    init_fn(sess)
+                    np_image, probabilities = sess.run([image, probabilities])
+                    probabilities = probabilities[0, 0:]
+                    sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x:x[1])]
+                    self.disp_names(sorted_inds,probabilities,include_background=False)
+                    
+                plt.figure()
+                plt.imshow(np_image.astype(np.uint8))
+                plt.axis('off')
+                plt.title(img_path)
+                plt.show()
         return
     def run(self):
-        self.use_inceptionv4()
+#         self.use_inceptionv4()
+        self.use_vgg16()
         
         return
     
