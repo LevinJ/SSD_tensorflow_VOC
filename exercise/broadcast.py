66e75c694e5039e907e65c4c72060b3060642c58
Levin Jian <jianzhirong@gmail.com>
2017-04-28 17:15:01 +0800

    match anchors via numpy

--- exercise/broadcast.py
+++ exercise/broadcast.py
@@ -1,13 +1,29 @@
 import numpy as np
 
-mask = np.array([True, True]).reshape(-1,1)
-mask = np.repeat(mask, [1,2], axis = 1)
+inds = [2,5,8]
 
-res = np.where(mask,
-     [[1, 2], [3, 4]],
-     [[9, 8], [7, 6]])
+gt_anchors_labels= np.zeros(20)
+gt_labels = np.arange(10)
 
-print(res)
+maskids = [1,2]
+
+gt_labels[maskids] = [110,100]
+
+# b = gt_labels>5
+# gt_labels[b] = [0,0,-1,-1]
+
+
+gt_anchors_labels[inds] = gt_labels[inds]
+
+print(gt_anchors_labels)
+# mask = np.array([False, True]).reshape(-1,1)
+# mask = np.repeat(mask, 2, axis = 1)
+# 
+# res = np.where(mask,
+#      [[1, 2], [3, 4]],
+#      [[9, 8], [7, 6]])
+# 
+# print(res)
 
 # a = np.array([1.0, 2.0, 3.0])
 # b = 2.0
@@ -62,22 +78,39 @@ def compute_jaccard(gt_bboxes, anchors):
     print(jaccard)
     return jaccard
 
+
+
+
 def match_achors(gt_labels, gt_bboxes, anchors,jaccard, matching_threshold = 0.5):
-    num_bbox = gt_bboxes.shape[0]
-    gt_anchor_labels = np.zeros(num_bbox)
-    gt_anchor_ymins = np.zeros(num_bbox)
-    gt_anchor_xmins = np.zeros(num_bbox)
-    gt_anchor_ymaxs = np.ones(num_bbox)
-    gt_anchor_xmaxs = np.ones(num_bbox)
-    gt_anchor_bboxes = np.hstack([gt_anchor_ymins,gt_anchor_xmins,gt_anchor_ymaxs,gt_anchor_xmaxs])
+    num_anchors= jaccard.shape[1]
+    gt_bboxes = np.squeeze(gt_bboxes)
+    gt_anchor_labels = np.zeros(num_anchors)
+    gt_anchor_ymins = np.zeros(num_anchors)
+    gt_anchor_xmins = np.zeros(num_anchors)
+    gt_anchor_ymaxs = np.ones(num_anchors)
+    gt_anchor_xmaxs = np.ones(num_anchors)
+    gt_anchor_bboxes = np.hstack([gt_anchor_ymins.reshape(-1,1),gt_anchor_xmins.reshape(-1,1),gt_anchor_ymaxs.reshape(-1,1),gt_anchor_xmaxs.reshape(-1,1)])
+    
     
     
     mask = np.max(jaccard, axis = 0) > matching_threshold
+    mask_inds = np.argmax(jaccard, axis = 0)
+    mask_inds = mask_inds[mask]
+    gt_anchor_labels[mask] = gt_labels[mask_inds]
+    gt_anchor_bboxes[mask] = gt_bboxes[mask_inds]
     
-    gt_anchor_labels = np.where(mask, gt_labels, gt_anchor_labels)
-    gt_anchor_bboxes = np.where(gt_anchor_bboxes, )
     
-    return
+
+    
+    inds = np.argmax(jaccard, axis = 1)
+    
+    gt_anchor_labels[inds] = gt_labels
+    gt_anchor_bboxes[inds] = gt_bboxes[np.arange(len(inds))]
+    
+    
+    
+    
+    return gt_anchor_labels, gt_anchor_bboxes
 
 gt_bboxes = np.array([[0,0,1,2],[1,0,3,4]]).reshape((-1,1,4))
 gt_labels = np.array([1,2])
@@ -85,6 +118,8 @@ anchors = np.array([[100,100,105,105],[2,1,3,3.5],[0,0,10,10]]).reshape((1,-1,4)
 
 
 jaccard = compute_jaccard(gt_bboxes, anchors)
+gt_anchor_bboxes = match_achors(gt_labels, gt_bboxes, anchors,jaccard,matching_threshold = 0.01)
+print(gt_anchor_bboxes)
 
 
 
