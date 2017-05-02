ffe9e93d8216dbecdbb5446ad9ef70c69db68f66
Levin Jian <jianzhirong@gmail.com>
2017-05-03 06:06:20 +0800

    add color distortion, push test mAP from 0.3 to 0.37

--- preprocessing/ssd_vgg_preprocessing.py
+++ preprocessing/ssd_vgg_preprocessing.py
@@ -278,11 +278,11 @@ def preprocess_for_train(image, labels, bboxes,
         dst_image, bboxes = tf_image.random_flip_left_right(dst_image, bboxes)
 
         # Randomly distort the colors. There are 4 ways to do it.
-#         dst_image = apply_with_random_selector(
-#                 dst_image,
-#                 lambda x, ordering: distort_color(x, ordering, fast_mode),
-#                 num_cases=4)
-#         tf_summary_image(dst_image, bboxes, 'image_color_distorted')
+        dst_image = apply_with_random_selector(
+                dst_image,
+                lambda x, ordering: distort_color(x, ordering, fast_mode),
+                num_cases=4)
+        tf_summary_image(dst_image, bboxes, 'image_color_distorted')
 
         # Rescale to VGG input scale.
         image = dst_image * 255.
